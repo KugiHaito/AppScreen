deb10e57bc3cf14ca617454c3f2d217553406ab1
KugiHaito <carlgi99@hotmail.com>
2017-01-19 20:42:48 -0300

    New updates release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Essential Packages
-import MySQLdb as mysql
+import os
+import pymysql as mysql
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -21,12 +22,14 @@ Builder.load_string("""
 <Login>:
     BoxLayout:
         orientation:'vertical'
+        padding:10
         Label:
             text: 'Seja Bem Vindo'
-            font_size: 40
+            font_size: 50
             size_hint:(1, .4)
         TextInput:
             id:nick
+            text:open('.saved/.name.txt','r').read()
             hint_text: 'Nickname'
             font_size:20
             write_tab:False
@@ -34,6 +37,7 @@ Builder.load_string("""
             background_color:(0,0,0,0)
         TextInput:
             id:pswd
+            text:open('.saved/.pass.txt','r').read()
             hint_text: 'Password'
             password:True
             font_size:20
@@ -41,6 +45,16 @@ Builder.load_string("""
             size_hint:(1, .1)
             background_color:(0,0,0,0)
         Label:
+    BoxLayout:
+        orientation:'horizontal'
+        size_hint:(.05,1.2)
+        padding:5
+        CheckBox:
+            id:check
+            size_hint:(.1,1)
+        Label:
+            text:'Lembrar-me'
+            size_hint:(0,1)
     BoxLayout:
         orientation:'horizontal'
         size_hint: (1,.1)
@@ -51,7 +65,7 @@ Builder.load_string("""
             border: (2,5,2,5)
             #background_down:'img/btn.png'
             background_normal:'img/btn.png'
-            on_press: root.log(nick.text, pswd.text)
+            on_press: root.log(nick.text, pswd.text, check.active)
         Button:
             text: 'Cadastrar-se'
             size_hint: (1,.7)
@@ -64,17 +78,41 @@ Builder.load_string("""
     BoxLayout:
         orientation:'vertical'
         Label:
-            text: 'Seja Bem Vindo!'
+            text:"Seja Bem Vindo!"
             font_size:120
         Button:
             text: 'Logout'
-            size_hint:(1,.05)
-            on_press: root.manager.current = 'main'
+            size_hint:(1, .05)
+            on_press: root.quit()
 """)
+
+# Definition General
+try:
+    os.mkdir(".saved/")
+    _name = open(".saved/.name.txt", "w")
+    _name.close()
+    _pass = open(".saved/.pass.txt", "w")
+    _pass.close()
+    os.system("attrib +h .saved/")
+except:
+    pass
+
+def addslashes(s):
+    d = {'"':'\\"', "'":"\\'", "\0":"\\\0", "\\":"\\\\"}
+    return ''.join(d.get(c, c) for c in s)
 
 class Login(Screen):
     # Functions
-    def log(args, n, p):
+    def pull():
+        _name = open(".saved/.name.txt", "r")
+        ni = _name.read()
+        _name.close()
+        _pass = open(".saved/.pass.txt", "r")
+        p = _pass.read()
+        _pass.close()
+        return ni
+
+    def log(args, n, p, c):
         if n == "Nickname.." or n == "":
             p = Popup(title='Login Error', content=Label(text="Digite seu apelido!", color=(1,0,0,1)),size_hint=(.6, .2))
             p.open()
@@ -82,9 +120,22 @@ class Login(Screen):
             p = Popup(title='Login Error', content=Label(text="Digite sua senha!", color=(1,0,0,1)),size_hint=(.6, .2))
             p.open()
         else:
-            sql = "select * from users where nickname ='"+n+"' and passwd='"+p+"'"
+            name = addslashes(n)
+            pswd = addslashes(p)
+            sql = "select * from users where nickname ='"+name+"' and passwd='"+pswd+"'"
             enc = cur.execute(sql)
             if enc:
+                if c:
+                    try:
+                        _name = open(".saved/.name.txt", "w")
+                        _name.write(name)
+                        _name.close()
+                        _pass = open(".saved/.pass.txt", "w")
+                        _pass.write(pswd)
+                        _pass.close()
+                        os.system("attrib +h .saved/")
+                    except:
+                        print("Error in saving..")
                 sm.current = 'space'
                 Window.size=(1366, 768)
             else:
@@ -99,18 +150,22 @@ class Login(Screen):
             p = Popup(title='Login Error', content=Label(text="Digite sua senha!", color=(1,0,0,1)),size_hint=(.6, .2))
             p.open()
         else:
-            sql = "select * from users where nickname = '"+n+"'"
+            # Tratando dados..
+            name = addslashes(n)
+            pswd = addslashes(p)
+            sql = "select * from users where nickname = '"+name+"'"
             rows = cur.execute(sql)
-        if rows > 0:
-            p = Popup(title='Login Error', content=Label(text="Este apelido esta em uso!", color=(1,0,0,1)),size_hint=(.6, .2))
-            p.open()
-        else:
-            sql = "insert into users (nickname, passwd, level, photo, online) values ('"+n+"', '"+p+"', 'Usuario', default, '0')"
-            cur.execute(sql)
-            con.commit()
+            if rows > 0:
+                p = Popup(title='Login Error', content=Label(text="Este apelido esta em uso!", color=(1,0,0,1)),size_hint=(.6, .2))
+                p.open()
+            else:
+                sql = "insert into users (nickname, passwd, level, photo, online) values ('"+name+"', '"+pswd+"', 'Usuario', default, '0')"
+                cur.execute(sql)
+                con.commit()
 
 class Home(Screen):
-    pass
+    def quit(arg):
+        sm.current = 'main'
 
 # Create the screen manager
 sm = ScreenManager()
