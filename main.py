3019eeb58aa6a9b5ee185c5e2892c7856cc0a58e
KugiHaito <kugihaito99@gmail.com>
2018-03-01 18:40:39 -0300

    Fix main added

---
+++ main.py
@@ -0,0 +1,18 @@
+"""Main Documentation.
+
+This main uses as a simple example, the creation of a blank application.
+
+----
+
+See other examples read the documentation:
+
+on `Github <http://github.com/KugiHaito/AppScreen>`_
+
+"""
+from appscreen.layout import Blank
+
+# set blank app
+app = Blank()
+
+# run application
+app.run()
